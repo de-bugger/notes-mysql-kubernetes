86e82216d8a3593d19109d3b28036966eac71009
de-bugger <darshgupta50@gmail.com>
2019-03-11 17:04:11 +0530

    files changed

--- src/main/java/com/darsh/demo/controller/NotesController.java
+++ src/main/java/com/darsh/demo/controller/NotesController.java
@@ -2,12 +2,9 @@ package com.darsh.demo.controller;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Map;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
